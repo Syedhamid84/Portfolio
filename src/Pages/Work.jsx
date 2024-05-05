e9b0e7aadda43d939828b963ec1ee4b860d00567
CodeSprint01 <syedhamidali84@gmail.com>
2024-05-06 02:10:52 +0500

    bebal fixes

--- src/Pages/Work.jsx
+++ src/Pages/Work.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faArrowsToEye,
   faCode,
   faEye,
-  faFaceRollingEyes,
   faLaptopCode,
 } from "@fortawesome/free-solid-svg-icons";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
